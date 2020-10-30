aa8cebd396b77c5d1c7254ee1f82b09f9a3422ee
yuhengUA2077 <72234409+yuhengUA2077@users.noreply.github.com>
2020-10-29 18:18:03 -0600

    Update CityListTest.java

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -32,6 +32,6 @@ class CityListTest {
     @Test
     void testGetCities(){
         CityList cityList = mockCityList();
-        assertEquals(1, mockCity().compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
     }
 }
